b6807c7ccd095cf8d84608658d089adb0afe1277
akramansari1433 <akramansari1433@gmail.com>
2022-12-27 12:41:34 +0530

    added gatby puglin for html

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,25 @@ const config: GatsbyConfig = {
    // If you use VSCode you can also use the GraphQL plugin
    // Learn more at: https://gatsby.dev/graphql-typegen
    graphqlTypegen: true,
-   plugins: ["gatsby-plugin-postcss"],
+   plugins: [
+      {
+         resolve: "gatsby-plugin-minify-html",
+         options: {
+            debug: false, // debug optional, default false
+            config: {
+               collapseWhitespace: true,
+               minifyCSS: true,
+               minifyJS: false,
+               removeComments: true,
+               removeScriptTypeAttributes: false,
+               removeStyleLinkTypeAttributes: false,
+               sortAttributes: false,
+               useShortDoctype: false,
+            },
+         },
+      },
+      "gatsby-plugin-postcss",
+   ],
 };
 
 export default config;
